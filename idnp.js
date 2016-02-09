3ca6d8418d7d496b58c100a2959363d3c5a188b4
Alexey Bondarenko <alexeybondarenko@me.com>
2016-02-09 16:13:16 +0200

    Update idnp.js

--- idnp.js
+++ idnp.js
@@ -4,8 +4,9 @@ function idnpCheckSum(val, multiplier) {
   multiplier = multiplier.toString();
   return (val.toString().split('').reduce(function (prev, val, i) {
       return prev + (+val) * (+multiplier[i]);
-    }, 0) / 10) % 10;
+    }, 0) % 10);
 }
+
 
 function validateIdnp(val) {
 
@@ -16,7 +17,6 @@ function validateIdnp(val) {
   if (['0', '2'].indexOf(val.charAt(0)) == -1) return false;
 
   var multiplier = '731731731731';
-  var checkSum = idnpCheckSum(val.slice(0, val.length - 1), multiplier);
 
-  return true;
+  return idnpCheckSum(val.slice(0, val.length - 1), multiplier) === ~~val.charAt(val.length - 1);
 }
